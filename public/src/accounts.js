84081e9a7654c0dd3f29c7e6627667bf9bd2d65e
Christopher Y. Marquez <145067852+chris-y-marquez@users.noreply.github.com>
2023-10-03 18:44:34 -0700

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -3,23 +3,37 @@ function findAccountById(accounts, id) {
   return found
 }
 
+
 function sortAccountsByLastName(accounts) {
   let results = accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
   return results
   
 }
 
-function getTotalNumberOfBorrows(account, books) {
-  const id = account.id;
-  let results = books.filter((books) => id === books.borrowed.id);
-  return results
 
-  
-    
-  });
+function getTotalNumberOfBorrows(account, books) {
+  let count = 0 
+  books.forEach((book) => {
+    count += book.borrows.filter((borrow) => borrow.id === account.id).length;
+    });
+    return count
 }
 
-function getBooksPossessedByAccount(account, books, authors) {}
+  
+function getBooksPossessedByAccount(account, books, authors) {
+const checkedOut = books.filter((book) => {
+ return book.borrows.some((borrow) => borrow.returned === false && 
+ borrow.id === account.id);
+
+
+});
+const results = checkedOut.map((book) => {
+const author = authors.find((author) => book.authorId === author.id)
+return {...book, author}
+
+});
+  return results
+  }
 
 module.exports = {
   findAccountById,
