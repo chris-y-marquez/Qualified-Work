22ba8d76508f9b1b39509f1f1a2326b17b09c457
Christopher Y. Marquez <145067852+chris-y-marquez@users.noreply.github.com>
2023-10-03 18:45:02 -0700

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,30 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  let result = authors.find((authors) => authors.id === id);
+  return result
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  let result = books.find((books) => books.id === id);
+  return result
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const checkedOut = books.filter((books) => books.borrows[0].returned === false);
+  const returned = books.filter((books) => books.borrows[0].returned === true);
+  return [checkedOut, returned]
+}
 
-function getBorrowersForBook(book, accounts) {}
+
+function getBorrowersForBook(book, accounts) {
+  const borrowers = book.borrows.map((book) => {
+   const accountId = findAuthorById(accounts, book.id)
+   accountId.returned = book.returned 
+   return accountId;
+  })
+ .slice (0,10);
+ return borrowers
+}
+
 
 module.exports = {
   findAuthorById,
